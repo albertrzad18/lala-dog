4eb2a8c228d9838994282b81e58bcfd63a993b8e
Albert <s95551@pollub.edu.pl>
2025-04-26 21:17:24 +0200

    created navbar and template for all pages

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import HoverLink from "./HoverLink";
+
+const Navbar = () => {
+  return (
+    <nav className="fixed top-0 w-full h-72 md:h-36 bg-gray-300 z-50 shadow-md">
+      <div className="max-w-screen-xl mx-auto flex flex-col md:flex-row items-center justify-between p-4 md:p-6">
+        <ul className="flex flex-row gap-6 md:gap-32 text-3xl">
+          <li>
+            <HoverLink text="Informacje" link="#info" />
+          </li>
+          <li>
+            <HoverLink text="Galeria" link="#gallery" />
+          </li>
+        </ul>
+
+        <div className="my-4 md:my-0">
+          <img
+            src="dog.jpg"
+            className="w-24 md:w-48 rounded-full border-4 border-green-600"
+            alt="Logo"
+          />
+        </div>
+
+        <ul className="flex flex-row gap-6 md:gap-32 text-3xl">
+          <li>
+            <HoverLink text="Cennik" link="#pricing" />
+          </li>
+          <li>
+            <HoverLink text="Kontakt" link="#contact" />
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/pages/About.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../Navbar";
+
+const AboutPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="min-h-screen pt-32 bg-gradient-to-r from-[#bdc3c7] to-[#2c3e50] text-black text-8xl flex items-center justify-center">
+        O MNIE //TODO
+      </div>
+    </>
+  );
+};
+
+export default AboutPage;
