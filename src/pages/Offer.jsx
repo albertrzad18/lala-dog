dcc41109d65cd8d19d19f6ae61aaf70ea6ea391b
Albert <s95551@pollub.edu.pl>
2025-05-08 17:06:18 +0200

    modified offer page added price list imgs

--- src/pages/Offer.jsx
+++ src/pages/Offer.jsx
@@ -1,13 +1,28 @@
 import FadeInOut from "../components/FadeInOut";
 import Navbar from "../components/Navbar";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 const Offer = () => {
+  const [lightboxImg, setLightboxImg] = useState(null);
+
+  const openLightbox = (src) => {
+    setLightboxImg(src);
+  };
+
+  const closeLightbox = () => {
+    setLightboxImg(null);
+  };
+
   return (
     <>
       <Navbar />
       <FadeInOut>
-        <div className="pt-44 px-6 md:px-32 flex flex-col lg:flex-col items-center justify-center gap-12 lg:gap-16">
-          <div className="w-full lg:w-1/2 bg-gray-100 rounded-xl shadow-lg p-8 space-y-4 border-4 border-green-500 text-gray-800 h-full flex flex-col items-center justify-center text-center text-lg">
+        <div className="pt-44 px-6 md:px-32 flex flex-row items-stretch justify-center gap-12 lg:gap-16">
+          <div
+            className="w-full lg:w-1/2 bg-gray-100 rounded-xl shadow-lg p-8 space-y-3.5 border-4 border-green-500 text-gray-800 h-full flex flex-col items-center justify-center text-center
+          "
+          >
             <h2 className="text-2xl md:text-4xl font-bold text-green-700 mb-4">
               Dlaczego Lala Dog?
             </h2>
@@ -42,7 +57,7 @@ const Offer = () => {
               <h2 className="text-2xl md:text-4xl font-bold text-green-700 mb-4">
                 Oferta
               </h2>
-              <ul className="list-disc list-inside space-y-2 text-lg font-semibold">
+              <ul className="list-disc list-inside space-y-4 text-xl font-semibold">
                 <li>Kąpiele i pielęgnacja sierści</li>
                 <li>SPA dla psów</li>
                 <li>Strzyżenie psów</li>
@@ -56,6 +71,45 @@ const Offer = () => {
             </div>
           </div>
         </div>
+
+        <div className="flex flex-col items-center justify-center pt-32 px-6 md:px-32">
+          <h2 className="text-3xl md:text-5xl font-bold text-center text-green-700">
+            Cennik
+          </h2>
+          <div className="pt-16 px-6 md:px-32 flex flex-row items-center justify-center gap-12 lg:gap-16">
+            <img
+              src="priceList1.jpg"
+              alt="Price List 1"
+              className="w-full lg:w-1/2 rounded-xl shadow-lg border-4 border-green-500 cursor-pointer"
+              onClick={() => openLightbox("/priceList1.jpg")}
+            />
+            <img
+              src="priceList2.jpg"
+              alt="Price List 2"
+              className="w-full lg:w-1/2 rounded-xl shadow-lg border-4 border-green-500 cursor-pointer"
+              onClick={() => openLightbox("/priceList2.jpg")}
+            />
+          </div>
+        </div>
+        {lightboxImg && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.15 }}
+          >
+            <div
+              className="fixed inset-0 backdrop-blur-md bg-black/40 flex items-center justify-center z-50"
+              onClick={closeLightbox}
+            >
+              <img
+                src={lightboxImg}
+                alt="Large preview"
+                className="max-w-full max-h-full rounded-lg shadow-2xl"
+              />
+            </div>
+          </motion.div>
+        )}
       </FadeInOut>
     </>
   );
