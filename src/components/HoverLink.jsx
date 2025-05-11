4073d46df9ec5c018a74346bb5dab6e5608d1444
Albert <s95551@pollub.edu.pl>
2025-05-11 09:40:32 +0200

    choosed font finally

--- src/components/HoverLink.jsx
+++ src/components/HoverLink.jsx
@@ -1,7 +1,7 @@
 const HoverLink = ({ text, link }) => {
   return (
     <>
-      <button className="btn relative inline-flex items-center justify-start overflow-hidden font-medium font-mono transition-all group px-2.5  border-black mb-8 ">
+      <button className="btn relative inline-flex items-center justify-start overflow-hidden font-medium rowdies-bold transition-all group px-2.5  border-black mb-8 ">
         <span className="w-56 h-48 rounded bg-purple-500 absolute bottom-0 left-0 translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
         <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white p-2">
           <a href={link}>{text}</a>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,13 +6,13 @@ const AboutPage = () => {
     <>
       <Navbar />
       <FadeInOut>
-        <div className="pt-12 md:pt-36 flex flex-col items-center justify-end gap-24 relative text-white">
+        <div className="pt-12 md:pt-36 flex flex-col items-center justify-end gap-24 relative text-white ">
           <div className="flex flex-row justify-center flex-wrap w-screen h-[550px] md:h-[350px]">
             <div className="w-full md:w-2/3 lg:w-1/2 h-full rounded-xl p-8 space-y-6 overflow-auto">
               <h1 className="text-3xl md:text-4xl rowdies-bold text-[#7e54f0] text-center">
                 pSIEMKA! Tu Ala – albo po prostu Lala
               </h1>
-              <p className="text-center text-white font-mono text-xl">
+              <p className="text-center text-white text-xl rowdies-regular">
                 Tak mówią do mnie w rodzinie od dziecka i tak już zostało.
                 Zwierzętami interesowałam się odkąd tylko pamiętam. Mówią, że
                 miałam kota na punkcie psów — i to całkiem dosłownie! Przez całe
@@ -35,7 +35,7 @@ const AboutPage = () => {
             alt="Lala"
           />
           <div className="w-full md:w-2/3 lg:w-1/2 h-full rounded-xl p-8 space-y-6 overflow-auto">
-            <p className="pt-12 text-center text-white font-mono text-xl">
+            <p className="pt-12 text-center text-white rowdies-regular text-xl">
               Jako nastolatka postanowiłam pójść za głosem serca — wyprowadziłam
               się z rodzinnego miasta, by zdobywać wiedzę i doświadczenie.
               Skończyłam technikum weterynaryjne w Trzcianie koło Rzeszowa, a
@@ -45,7 +45,7 @@ const AboutPage = () => {
           </div>
           <div className="flex flex-col md:flex-row justify-center items-center flex-wrap w-screen bg-purple-800 h-[810px] md:h-[460px]">
             <div className="w-full md:w-2/3 lg:w-1/2 h-full rounded-xl p-8 space-y-6 overflow-auto">
-              <p className="text-center text-white font-mono text-xl">
+              <p className="text-center text-white rowdies-regular text-xl">
                 Później wróciłam do rodzinnego miasta i ukończyłam studia
                 magisterskie na kierunku analityka weterynaryjna na
                 Uniwersytecie Przyrodniczym w Lublinie. Przez kilka lat
@@ -53,14 +53,14 @@ const AboutPage = () => {
                 mi... zwierząt!
               </p>
 
-              <p className="text-center text-white font-mono text-xl">
+              <p className="text-center text-white rowdies-regular text-xl">
                 I tak, trochę z marzeń, a bardziej z serca, trafiłam do
                 groomingu. Ukończyłam podstawowy kurs groomerski w Dog House
                 Grooming Academy w Krakowie i poczułam, że jestem dokładnie tam,
                 gdzie powinnam być już dawno.
               </p>
 
-              <p className="text-center text-white font-mono text-xl">
+              <p className="text-center text-white rowdies-regular text-xl">
                 Od tamtej pory nieustannie się szkolę, poszerzam wiedzę i
                 zdobywam nowe kwalifikacje, żeby każdy pies oraz kot wychodził z
                 mojego salonu piękny, szczęśliwy i merdający ogonem.
@@ -78,8 +78,8 @@ const AboutPage = () => {
               className="w-88 h-88 rounded-full p-8 hidden md:block"
               alt="Lala"
             />
-            <div className="w-full md:w-2/3 lg:w-1/2 h-full rounded-xl p-8 space-y-6overflow-auto">
-              <p className="text-center text-white font-mono text-xl">
+            <div className="w-full md:w-2/3 lg:w-1/2 h-full rounded-xl p-8 space-y-6 overflow-auto">
+              <p className="text-center text-white rowdies-regular text-xl">
                 Tytuł technika weterynarii i studia z analityki weterynaryjnej
                 dają mi solidne zaplecze – wiem, jak zadbać nie tylko o wygląd
                 pupila, ale i o jego zdrowie, komfort i dobre samopoczucie. Na
@@ -87,7 +87,7 @@ const AboutPage = () => {
                 dbać o sierść, jak poprawić komfort pupila, co warto zmienić,
                 żeby wszystkim żyło się lepiej.
               </p>
-              <p className="text-center text-white font-mono text-xl">
+              <p className="text-center text-white rowdies-regular text-xl">
                 Nie tylko psy rasowe wymagają i zasługują na profesjonalną
                 pielęgnację w salonie groomerskim — każdy pies, bez względu na
                 “rasę”, powinien czuć się zadbany i kochany, a że sama jestem
@@ -99,7 +99,7 @@ const AboutPage = () => {
                 żeby pilnować porządku i czy po prostu się obijać i cieszyć
                 psim, beztroskim życiem.
               </p>
-              <p className="text-center text-white font-mono text-xl">
+              <p className="text-center text-white rowdies-regular text-xl">
                 Z miłości do psiej i kociej urody właśnie powstał ten salon –
                 Lala Dog.
               </p>
