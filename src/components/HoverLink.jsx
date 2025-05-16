fc7afce50f1cd8518a6a134d6df75809a8a1646f
Albert <s95551@pollub.edu.pl>
2025-05-16 17:17:19 +0200

    href -> react link

--- src/components/HoverLink.jsx
+++ src/components/HoverLink.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 const HoverLink = ({ text, link }) => {
   return (
     <>
       <button className="btn relative inline-flex items-center justify-start overflow-hidden font-medium rowdies-bold transition-all group px-2.5 mb-8 ">
         <span className="w-56 h-48 rounded bg-purple-500 absolute bottom-0 left-0 translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
         <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white p-2">
-          <a href={link}>{text}</a>
+          <Link to={link}>{text}</Link>
         </span>
       </button>
     </>
