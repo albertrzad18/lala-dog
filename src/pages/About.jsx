8d434f741742de8ac12f2a4cf84addda0b9dd2b6
Albert <s95551@pollub.edu.pl>
2025-05-17 08:45:34 +0200

    new about me page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,13 +6,13 @@ const AboutPage = () => {
     <>
       <Navbar />
       <FadeInOut>
-        <div className="pt-12 md:pt-52 flex flex-col items-center justify-end gap-24 relative text-white ">
-          <div className="flex flex-row justify-center flex-wrap w-screen h-[550px] md:h-[370px]">
-            <div className="w-full md:w-2/3 lg:w-1/2 h-full rounded-xl p-8 space-y-6 overflow-auto">
-              <h1 className="text-3xl md:text-4xl rowdies-bold text-[#7e54f0] text-center">
+        <div className="pt-12 md:pt-28">
+          <div className="w-full h-full font-semibold p-4 pt-8 space-y-4 space-x-8 md:space-y-0 flex flex-col md:flex-row items-center justify-center text-white text-center md:text-left md:gap-8">
+            <div className="w-full md:w-2/3 lg:w-1/2 space-y-4 break-words text-pretty">
+              <h2 className="text-2xl md:text-4xl rowdies-bold text-[#7e54f0] mb-4">
                 pSIEMKA! Tu Ala – albo po prostu Lala
-              </h1>
-              <p className="text-center text-white text-xl rowdies-regular">
+              </h2>
+              <p className="rowdies-regular text-white text-center">
                 Tak mówią do mnie w rodzinie od dziecka i tak już zostało.
                 Zwierzętami interesowałam się odkąd tylko pamiętam. Mówią, że
                 miałam kota na punkcie psów — i to całkiem dosłownie! Przez całe
@@ -23,87 +23,81 @@ const AboutPage = () => {
             </div>
             <img
               src="about/circle1.png"
-              className="w-80 h-84 rounded-full hidden md:block shadow-xl shadow-[#7e54f0]"
+              className="w-44 h-54 sm:w-52 sm:h-72 md:w-60 md:h-80 rounded-full shadow-xl shadow-[#7e54f0]"
               alt="Lala"
             />
           </div>
         </div>
-        <div className="flex flex-row justify-center items-center flex-wrap w-screen bg-purple-400 h-[450px] md:h-[360px]">
+        <div className="w-full h-full font-semibold p-4 pt-4 space-y-4 space-x-8 md:space-y-0 flex flex-col md:flex-row items-center justify-center bg-purple-400 text-white text-center md:text-left md:gap-8">
           <img
             src="about/circle2.jpeg"
-            className="w-80 h-80 rounded-full hidden md:block shadow-xl shadow-[#7e54f0]"
+            className="order-2 md:order-1 w-52 h-52 sm:w-60 sm:h-60 md:w-70 md:h-70 rounded-full shadow-xl shadow-[#7e54f0]"
             alt="Lala"
           />
-          <div className="w-full md:w-2/3 lg:w-1/2 h-full rounded-xl p-8 space-y-6 overflow-auto">
-            <p className="pt-20 text-center text-white rowdies-regular text-xl">
+
+          <div className="order-1 md:order-2 w-full md:w-2/3 lg:w-1/2 space-y-4 break-words text-pretty">
+            <p className="text-white rowdies-regular text-center">
               Jako nastolatka postanowiłam pójść za głosem serca — wyprowadziłam
               się z rodzinnego miasta, by zdobywać wiedzę i doświadczenie.
               Skończyłam technikum weterynaryjne w Trzcianie koło Rzeszowa, a
               uzyskany tytuł technika weterynarii tylko utwierdził mnie w
-              przekonaniu, że to właśnie moja droga.
+              przekonaniu, że to właśnie moja droga. Później wróciłam do
+              rodzinnego miasta i ukończyłam studia magisterskie na kierunku
+              analityka weterynaryjna na Uniwersytecie Przyrodniczym w Lublinie.
             </p>
           </div>
-          <div className="flex flex-col md:flex-row justify-center items-center flex-wrap w-screen bg-purple-800 h-[810px] md:h-[460px]">
-            <div className="w-full md:w-2/3 lg:w-1/2 h-full rounded-xl p-8 space-y-6 overflow-auto">
-              <p className="text-center text-white rowdies-regular text-xl">
-                Później wróciłam do rodzinnego miasta i ukończyłam studia
-                magisterskie na kierunku analityka weterynaryjna na
-                Uniwersytecie Przyrodniczym w Lublinie. Przez kilka lat
-                pracowałam w laboratorium, ale czegoś mi brakowało. Brakowało
-                mi... zwierząt!
-              </p>
+        </div>
 
-              <p className="text-center text-white rowdies-regular text-xl">
-                I tak, trochę z marzeń, a bardziej z serca, trafiłam do
-                groomingu. Ukończyłam podstawowy kurs groomerski w Dog House
-                Grooming Academy w Krakowie i poczułam, że jestem dokładnie tam,
-                gdzie powinnam być już dawno.
-              </p>
-
-              <p className="text-center text-white rowdies-regular text-xl">
-                Od tamtej pory nieustannie się szkolę, poszerzam wiedzę i
-                zdobywam nowe kwalifikacje, żeby każdy pies oraz kot wychodził z
-                mojego salonu piękny, szczęśliwy i merdający ogonem.
-              </p>
-            </div>
-            <img
-              src="about/circle3.png"
-              className="w-80 h-80 rounded-full hidden md:block shadow-xl shadow-[#7e54f0]"
-              alt="Lala"
-            />
+        <div className="w-full h-full font-semibold p-4 pt-8 space-y-4 space-x-8 md:space-y-0 flex flex-col md:flex-row items-center justify-center bg-purple-600 text-white text-center md:text-left md:gap-8">
+          <div className="w-full md:w-2/3 lg:w-1/2 space-y-4 break-words text-pretty">
+            <p className="text-white rowdies-regular text-center">
+              Przez kilka lat pracowałam w laboratorium, ale czegoś mi
+              brakowało. Brakowało mi... zwierząt! I tak, trochę z marzeń, a
+              bardziej z serca, trafiłam do groomingu. Ukończyłam podstawowy
+              kurs groomerski w Dog House Grooming Academy w Krakowie i
+              poczułam, że jestem dokładnie tam, gdzie powinnam być już dawno.
+              Od tamtej pory nieustannie się szkolę, poszerzam wiedzę i zdobywam
+              nowe kwalifikacje, żeby każdy pies oraz kot wychodził z mojego
+              salonu piękny, szczęśliwy i merdający ogonem.
+            </p>
           </div>
-          <div className="flex flex-col md:flex-row justify-center items-center w-screen bg-purple-950 h-[1080px] md:h-[535px]">
-            <img
-              src="about/circle4.jpeg"
-              className="w-80 h-80 rounded-full hidden md:block shadow-xl shadow-[#7e54f0]"
-              alt="Lala"
-            />
-            <div className="w-full md:w-2/3 lg:w-1/2 h-full rounded-xl p-8 space-y-6 overflow-auto">
-              <p className="text-center text-white rowdies-regular text-xl">
-                Tytuł technika weterynarii i studia z analityki weterynaryjnej
-                dają mi solidne zaplecze – wiem, jak zadbać nie tylko o wygląd
-                pupila, ale i o jego zdrowie, komfort i dobre samopoczucie. Na
-                co dzień nie tylko pielęgnuję, ale też chętnie doradzam – jak
-                dbać o sierść, jak poprawić komfort pupila, co warto zmienić,
-                żeby wszystkim żyło się lepiej.
-              </p>
-              <p className="text-center text-white rowdies-regular text-xl">
-                Nie tylko psy rasowe wymagają i zasługują na profesjonalną
-                pielęgnację w salonie groomerskim — każdy pies, bez względu na
-                “rasę”, powinien czuć się zadbany i kochany, a że sama jestem
-                “psią mamą” dwóch kundelków to właśnie kundelki zajmują w moim
-                sercu szczególne miejsce.Lagera i Dorotkę możecie poznać
-                wirtualnie na ich własnej podstronie (tak, mają więcej fanów niż
-                ja!), a jeśli będziecie mieć szczęście, przywitają Was też
-                osobiście w salonie — w końcu czasem wpadają ze mną do pracy,
-                żeby pilnować porządku i czy po prostu się obijać i cieszyć
-                psim, beztroskim życiem.
-              </p>
-              <p className="text-center text-white rowdies-regular text-xl">
-                Z miłości do psiej i kociej urody właśnie powstał ten salon –
-                Lala Dog.
-              </p>
-            </div>
+          <img
+            src="about/circle3.png"
+            className="w-52 h-52 sm:w-60 sm:h-60 md:w-70 md:h-70 rounded-full shadow-xl shadow-[#7e54f0]"
+            alt="Lala"
+          />
+        </div>
+
+        <div className="w-full h-full font-semibold p-4 pt-8 space-y-4 space-x-8 md:space-y-0 flex flex-col md:flex-row items-center justify-center bg-purple-800 text-white text-center md:text-left md:gap-8">
+          <img
+            src="about/circle4.jpeg"
+            className="order-2 md:order-1 w-52 h-52 sm:w-60 sm:h-60 md:w-70 md:h-70 rounded-full shadow-xl shadow-[#7e54f0]"
+            alt="Lala"
+          />
+          <div className="order-1 md:order-2 w-full md:w-2/3 lg:w-1/2 space-y-4 break-words text-pretty">
+            <p className="text-white rowdies-regular text-center">
+              Tytuł technika weterynarii i studia z analityki weterynaryjnej
+              dają mi solidne zaplecze – wiem, jak zadbać nie tylko o wygląd
+              pupila, ale i o jego zdrowie, komfort i dobre samopoczucie. Na co
+              dzień nie tylko pielęgnuję, ale też chętnie doradzam – jak dbać o
+              sierść, jak poprawić komfort pupila, co warto zmienić, żeby
+              wszystkim żyło się lepiej. Nie tylko psy rasowe wymagają i
+              zasługują na profesjonalną pielęgnację w salonie groomerskim —
+              każdy pies, bez względu na “rasę”, powinien czuć się zadbany i
+              kochany, a że sama jestem “psią mamą” dwóch kundelków to właśnie
+              kundelki zajmują w moim sercu szczególne miejsce.
+            </p>
+            <p className="text-white rowdies-regular text-center">
+              Lagera i Dorotkę możecie poznać wirtualnie na ich własnej
+              podstronie (tak, mają więcej fanów niż ja!), a jeśli będziecie
+              mieć szczęście, przywitają Was też osobiście w salonie — w końcu
+              czasem wpadają ze mną do pracy, żeby pilnować porządku i czy po
+              prostu się obijać i cieszyć psim, beztroskim życiem.
+            </p>
+            <p className="text-white rowdies-regular text-center">
+              Z miłości do psiej i kociej urody właśnie powstał ten salon – Lala
+              Dog.
+            </p>
           </div>
         </div>
       </FadeInOut>
